1262afb6696cafb10833de77328982ca4d2587cc
HongnamKim <ghdskawkd@naver.com>
2024-10-21 17:41:45 +0900

    init commit

---
+++ netflix/src/movies/movies.controller.ts
@@ -0,0 +1,43 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+} from '@nestjs/common';
+import { MoviesService } from './movies.service';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
+
+@Controller('movies')
+export class MoviesController {
+  constructor(private readonly moviesService: MoviesService) {}
+
+  @Get()
+  getMovies(@Query('title') title?: string) {
+    return this.moviesService.getManyMovies(title);
+  }
+
+  @Get(':id')
+  getMovie(@Param('id') id: string) {
+    return this.moviesService.getMovieById(+id);
+  }
+
+  @Post()
+  postMovie(@Body() body: CreateMovieDto) {
+    return this.moviesService.createMovie(body);
+  }
+
+  @Patch(':id')
+  patchMovie(@Param('id') id: string, @Body() body: UpdateMovieDto) {
+    return this.moviesService.updateMovie(+id, body);
+  }
+
+  @Delete(':id')
+  deleteMovie(@Param('id') id: string) {
+    return this.moviesService.deleteMovie(+id);
+  }
+}
