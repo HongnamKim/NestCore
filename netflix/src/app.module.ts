1e898ec09f3d74ea073bc06768b554721eeddd00
HongnamKim <ghdskawkd@naver.com>
2024-11-01 23:44:37 +0900

    PostgreSQL 설정, 환경변수 Joi Schema 검증

--- netflix/src/app.module.ts
+++ netflix/src/app.module.ts
@@ -1,7 +1,35 @@
 import { Module } from '@nestjs/common';
 import { MoviesModule } from './movies/movies.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [MoviesModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      // .env 파일의 스키마를 지정/검증
+      validationSchema: Joi.object({
+        ENV: Joi.string().required(),
+        DB_TYPE: Joi.string().required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_DATABASE: Joi.string().required(),
+      }),
+    }),
+    TypeOrmModule.forRoot({
+      type: process.env.DB_TYPE as 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [],
+      synchronize: true,
+    }),
+    MoviesModule,
+  ],
 })
 export class AppModule {}
--- netflix/src/movies/dto/update-movie.dto.ts
+++ netflix/src/movies/dto/update-movie.dto.ts
@@ -1,5 +1,10 @@
 import { IsNotEmpty, IsOptional } from 'class-validator';
 
+enum MovieGenre {
+  Fantasy = 'fantasy',
+  Action = 'action',
+}
+
 export class UpdateMovieDto {
   @IsNotEmpty()
   @IsOptional()
@@ -8,4 +13,37 @@ export class UpdateMovieDto {
   @IsNotEmpty()
   @IsOptional()
   genre?: string;
+
+  //@IsDefined() // 값이 반드시 전달 되어야함. null, undefined 불가능
+  //@IsBoolean()
+  //@IsOptional()
+  //@Validate(PasswordValidator)
+  //@IsPasswordValid()
+  //test: string;
 }
+
+/*@ValidatorConstraint()
+class PasswordValidator implements ValidatorConstraintInterface {
+  defaultMessage(validationArguments?: ValidationArguments): string {
+    return `비밀번호 길이는 4-8 $value`;
+  }
+
+  validate(
+    value: any,
+    validationArguments?: ValidationArguments,
+  ): Promise<boolean> | boolean {
+    // 비밀번호 길이는 4-8
+    return value.length > 4 && value.length < 8;
+  }
+}
+
+function IsPasswordValid(validationOptions?: ValidationOptions) {
+  return function (object: object, propertyName: string) {
+    registerDecorator({
+      target: object.constructor,
+      propertyName: propertyName,
+      options: validationOptions,
+      validator: PasswordValidator,
+    });
+  };
+}*/
