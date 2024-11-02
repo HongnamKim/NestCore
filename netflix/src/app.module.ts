87fabbce905515c4abd250932366689aefabf859
HongnamKim <ghdskawkd@naver.com>
2024-11-02 23:48:39 +0900

    Repository 적용

--- netflix/src/app.module.ts
+++ netflix/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { MoviesModule } from './movies/movies.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import * as Joi from 'joi';
+import { Movie } from './movies/entity/movie.entity';
 
 @Module({
   imports: [
@@ -10,8 +11,8 @@ import * as Joi from 'joi';
       isGlobal: true,
       // .env 파일의 스키마를 지정/검증
       validationSchema: Joi.object({
-        ENV: Joi.string().required(),
-        DB_TYPE: Joi.string().required(),
+        ENV: Joi.string().valid('dev', 'prod').required(),
+        DB_TYPE: Joi.string().valid('postgres').required(),
         DB_HOST: Joi.string().required(),
         DB_PORT: Joi.number().required(),
         DB_USERNAME: Joi.string().required(),
@@ -19,15 +20,18 @@ import * as Joi from 'joi';
         DB_DATABASE: Joi.string().required(),
       }),
     }),
-    TypeOrmModule.forRoot({
-      type: process.env.DB_TYPE as 'postgres',
-      host: process.env.DB_HOST,
-      port: +process.env.DB_PORT,
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-      entities: [],
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        type: configService.get<string>('DB_TYPE') as 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        entities: [Movie],
+        synchronize: true,
+      }),
+      inject: [ConfigService], // 비동기로 환경변수를 설정하기 위해 ConfigService 를 주입
     }),
     MoviesModule,
   ],
